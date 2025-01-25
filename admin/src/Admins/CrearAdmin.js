7095ba543138357c4791391f693850c05e393616
colibri <atorresa1800@alumno.ipn.mx>
2025-01-25 01:18:15 -0600

    Arregle un poco el diseniio

--- admin/src/Admins/CrearAdmin.js
+++ admin/src/Admins/CrearAdmin.js
@@ -18,32 +18,38 @@ formulario.addEventListener('submit', async function (event) {
 
         const pantallaCarga = document.getElementById('carga')
         console.log(pantallaCarga);
-        
-        pantallaCarga.classList.remove('d-none')
-        
-        const response = await fetch('/auth/agregarColaborador', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                nombre: formData.nombre.value,
-                pass: formData.contra.value,
-                apePa: formData.apePa.value,
-                apeMa: formData.apeMa.value,
-                email: formData.email.value,
-                fecha: formData.fecha.value
-            })
-        })
 
-        const data = await response.json()
-        //carga.classList.add('d-none')
-        console.log(data);
+        if (formData.contra.value === formData.contraC.value) {
+            pantallaCarga.classList.remove('d-none')
         
-        if (data[0][0].affectedRows == 1) {
-            pantallaCarga.classList.add('d-none')
-            window.location.href = '/admins'
+            const response = await fetch('/auth/agregarColaborador', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nombre: formData.nombre.value,
+                    pass: formData.contra.value,
+                    apePa: formData.apePa.value,
+                    apeMa: formData.apeMa.value,
+                    email: formData.email.value,
+                    fecha: formData.fecha.value
+                })
+            })
+
+            const data = await response.json()
+            //carga.classList.add('d-none')
+            console.log(data);
+            
+            if (data[0][0].affectedRows == 1) {
+                pantallaCarga.classList.add('d-none')
+                window.location.href = '/admins'
+            }
+        } else {
+            alert('La contraseña no coincide!!!')
         }
+        
+        
 
     } catch (error) {
         console.log(error);
